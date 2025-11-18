caab328a31c97c2647a7330f91035da107e0f263
lakshmipriyamb <lakshmipriyamb2002@gmail.com>
2025-11-18 12:56:11 +0530

    redux cart v3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,19 +15,19 @@ function Footer() {
             <p>Currently v5.3.2.</p>
         </div>
         <div className='col-md-3'>
-          <h5 className="fw-bold text-white"><FontAwesomeIcon icon={faCartShopping}/>Links</h5>
+          <h5 className="fw-bold text-white">Links</h5>
             <Link to={'/'} className='text-decoration-none text-light'>Landing Page</Link><br />
             <Link to={'/'} className='text-decoration-none text-light'>Home Page</Link><br />
             <Link to={'/history'} className='text-decoration-none text-light'>Watch History Page</Link>
         </div>
         <div className='col-md-3'>
-          <h5 className="fw-bold text-white"><FontAwesomeIcon icon={faCartShopping}/>Guides</h5>
+          <h5 className="fw-bold text-white">Guides</h5>
             <Link className='text-decoration-none text-light'>React</Link><br />
             <Link className='text-decoration-none text-light'>React Bootstrap</Link><br />
             <Link className='text-decoration-none text-light'>React Router</Link>
         </div>
         <div className='col-md-3'>
-          <h5 className="fw-bold text-white"><FontAwesomeIcon icon={faCartShopping}/>Contact Us</h5>
+          <h5 className="fw-bold text-white">Contact Us</h5>
             <div className='d-flex align-items-center'><input type="text" className='form-control' placeholder='Enter your email here' /> <FontAwesomeIcon icon={faArrowRight} className='fw-bold fs-3'/></div>
             <div className="d-flex justify-content-between align-items-center pe-4 py-3"><FontAwesomeIcon icon={faTwitter}/><FontAwesomeIcon icon={faInstagram}/><FontAwesomeIcon icon={faFacebook}/><FontAwesomeIcon icon={faLinkedin}/><FontAwesomeIcon icon={faGithub}/><FontAwesomeIcon icon={faPhone}/></div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../components/Header'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllProducts } from '../redux/slices/productSlice'
 
 
 function Home() {
+  const dispatch = useDispatch()
+  const {loading,allProducts,error} = useSelector(state=>state.productReducer)
+  console.log(allProducts);
+  
+
+  useEffect(()=>{
+    dispatch(getAllProducts())
+  },[])
   return (
     <>
       <Header />
       <div className='container py-5'>
-        <div className="row my-5">
+        {
+          loading?
+          <div className='d-flex justify-content-center align-items-center my-5'><img  width="200px" src="https://media2.dev.to/dynamic/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fdkf5jym2t51hf098ppgs.gif" alt="Loading" /> <h1>Loading...</h1></div>
+          :
+          
+          <div className="row my-5">
           {/* duplicte */}
-          <div className="col-md-3 mb-2">
+          {
+            allProducts?.length>0?
+            allProducts?.map(product=>(
+
+            
+            <div key={product?.id} className="col-md-3 mb-2">
             {/* card -react bootsrap*/}
             <Card>
-              <Card.Img height={'250px'} variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrdUcIETG-GGcK-kqCp4ooz_vLYiPJ70Hs3A&s" />
+              <Card.Img height={'250px'} variant="top" src={product?.thumbnail} />
               <Card.Body className='text-center'>
-                <Card.Title>Title</Card.Title>
-                <Link to={'/products/id/view'} className='btn btn-secondary'>View More...</Link>
+                <Card.Title>{product?.title}</Card.Title>
+                <Link to={`/products/${product?.id}/view`} className='btn btn-secondary'>View More...</Link>
               </Card.Body>
             </Card>
           </div>
-        </div>
+          ))
+        :
+        <p className='fs-5 fw-bold mt-5'>Product Not Found!!!</p>
+        }
+        </div>}
       </div>
     </>
   )
